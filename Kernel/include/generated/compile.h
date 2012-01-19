3bfb74fc7c7d2c0df7653229459bd50dd820625b
Paul Blair <toadlife@gmail.com>
2012-01-19 11:37:10 -0800

    Initial commit of Samsung's EL30 open source package

---
+++ Kernel/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Mon Jan 16 18:34:29 KST 2012"
+#define LINUX_COMPILE_TIME "18:34:29"
+#define LINUX_COMPILE_BY ""
+#define LINUX_COMPILE_HOST "joohwankim-desktop"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
